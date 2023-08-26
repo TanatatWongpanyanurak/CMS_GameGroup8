8db2a95a000c5a187bf499931b73edcd0b9babd0
Panaikran <panaikran1@gmail.com>
2023-08-26 14:41:31 +0700

    Camera function

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float minZoomDist;
+    [SerializeField] private float maxZoomDist;
+
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float zoomModifier;
+
+    [SerializeField] private float moveSpeed;
+
+    private Camera cam;
+
+    public static CameraController instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+            zoomModifier = 0.01f;
+        if (Input.GetKey(KeyCode.X))
+            zoomModifier = -0.01f;
+
+        float dist = Vector3.Distance(transform.position, cam.transform.position);
+
+        if (dist < minZoomDist && zoomModifier > 0f)
+            return;
+        else if (dist > maxZoomDist && zoomModifier < 0f)
+            return;
+
+        cam.transform.position += cam.transform.forward * zoomModifier * zoomSpeed;
+    }
+}
