3c9d362408799cb01459a465a094f4a3763a7287
tniThanaphong <136888127+tniThanaphong@users.noreply.github.com>
2023-09-11 02:04:53 +0700

    Create Time Manager

---
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TimeManager : MonoBehaviour
+{
+    public static Action onHourchanged;
+    public static Action onMonthchanged;
+    public static Action onYearschanged;
+
+
+    public static int minute { get; private set; }
+    public static int hour { get; private set; }
+
+    public static int day { get; private set; }
+    public static int month { get; private set; }
+    public static int year { get; private set; }
+
+    private float TimeConvertRate = 1.0f;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hour = 0;  minute = 0; day = 1; month = 1; year = 2020;
+        timer = TimeConvertRate;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+
+        if (timer <= 0)
+        {
+            // change times
+            minute += 10;
+            if (minute >= 60)
+            {
+                onHourchanged?.Invoke();
+                minute = 0;
+                hour++;
+            }
+
+            // change days
+            if (hour >= 24)
+            {
+                day++; hour = 0;
+            }
+
+            // change month
+            if (month == 4 | month == 6 | month == 9 | month == 11)
+            {
+                if (day > 31)
+                {
+                    day = 1;
+                    month++;
+                    onMonthchanged?.Invoke();
+                }
+            }
+            else if (month == 2)
+            {
+                if (DateTime.IsLeapYear(year))
+                {
+                    if (day > 29)
+                    {
+                        day = 1;
+                        month++;
+                        onMonthchanged?.Invoke();
+                    }
+                }
+                else
+                {
+                    if (day > 28)
+                    {
+                        day = 1;
+                        month++;
+                        onMonthchanged?.Invoke();
+                    }
+                }
+            }
+            else
+            {
+                if (day > 30)
+                {
+                    day = 1;
+                    month++;
+                    onMonthchanged?.Invoke();
+                }
+            }
+
+            //change years
+            if(month > 12)
+            {
+                month = 1;
+                year++;
+                onYearschanged?.Invoke();
+            }
+
+            timer = TimeConvertRate;
+        }
+    }
+
+
+}
