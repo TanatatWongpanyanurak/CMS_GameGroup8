f2c0626d6705b39dec70c8df9368fdf83c441cb6
Thanaphong <ka.thanaphong_st@tni.ac.th>
2023-09-18 10:37:38 +0700

    set up main value

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    // main game valuable
+     public static int money ;
+     public static int population ;
+
     void Start()
     {
-        
+        // starting value
     }
 
     // Update is called once per frame
